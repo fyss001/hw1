efa93aa00ee48db27ffc755c7f3611b70f4cea0e
Peiran <peiranqi@usc.edu>
2024-01-13 16:52:32 -0800

    Part 1 done

--- split.cpp
+++ split.cpp
@@ -13,11 +13,40 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+void add_to_back(Node*&, Node*&);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  if(in==nullptr) return;
+  int value = in->value;
+  if(value&1){  //odd
+    Node* temp = new Node(value, nullptr);
+    if(odds==nullptr){
+      odds=temp;
+    }else{
+      add_to_back(odds, temp);
+    }
+  }else{  //even
+    Node* temp = new Node(value, nullptr);
+    if(evens==nullptr){
+      evens=temp;
+    }else{
+      add_to_back(evens, temp);
+    }   
+  }
+  Node* prev = in;
+  in = in->next;
+  split(in, odds, evens);
+  delete prev;
 }
 
 /* If you needed a helper function, write it here */
+void add_to_back(Node*& curr, Node*& toAdd){
+  if(curr->next!=nullptr){
+    add_to_back(curr->next, toAdd);
+    return;
+  }else{
+    curr->next = toAdd;
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,44 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
-
+#include<iostream>
+using namespace std;
 int main(int argc, char* argv[])
 {
-
+    Node *head= new Node(1,nullptr);
+    Node *curr=head;
+    Node *odd = nullptr;
+    Node *even = nullptr;
+    for(int i=2;i<=10;i++){
+        curr->next = new Node(i, nullptr);
+        curr = curr->next;
+    }
+    split(head, odd, even);
+    cout<<(odd->value)<<endl;
+    if(even==nullptr){
+        cout<<"Even list is empty\n";
+    }
+    cout<<(even->value)<<endl;
+    for(int i=1;i<=5;i++){
+        cout<<(odd->value)<<endl;
+        auto temp=odd;
+        odd=odd->next;
+        delete temp;
+    }
+    for(int i=1;i<=5;i++){
+        cout<<(even->value)<<endl;
+        auto temp=even;
+        even=even->next;
+        delete temp;
+    }
+    if(head==nullptr){
+        cout<<"head is set to null.\n";
+    }
+    if(even==nullptr){
+        cout<<"Even list is empty\n";
+    }
+    if(odd==nullptr){
+        cout<<"Odd list is empty\n";
+    }
+    return 0;
 }
